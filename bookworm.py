1979e12fffbd9dcdc9d034ff58e29bcedd1c5b8d
Scott W. Davis <zscottywd@msn.com>
2017-10-28 12:49:31 -0700

    Add files via upload

---
+++ bookworm.py
@@ -0,0 +1,375 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# Bookworm
+
+
+## Overview
+
+In this project, you will build a simple question-answering agent that is able to learn from any text data you provide, and answer queries posed in natural language. You will use IBM Watson's cloud-based services to process the input text data and find relevant responses.
+
+
+
+## Getting Started
+
+In order to use Watson's cloud-based services, you first need to create an account on the [IBM Bluemix platform](https://console.ng.bluemix.net/).
+
+<div>
+    <div style="display: table-cell; width: 50%;">
+        <img src="images/watson-logo.png" alt="IBM Watson logo" width="200" />
+    </div>
+    <div style="display: table-cell; width: 50%;">
+        <img src="images/bluemix-logo.png" alt="IBM Bluemix logo" width="400" />
+    </div>
+</div>
+
+Then, for each service you want to use, you have to create an instance of that service. You can continue with the tasks below, and create a service instance when indicated.
+
+
+# ## 1. Create and configure Discovery service
+# 
+# Create an instance of the **Discovery** service. You will use this to process a set of text documents, and _discover_ relevant facts and relationships.
+# 
+# - Go to the [IBM Bluemix Catalog](https://console.ng.bluemix.net/catalog/?taxonomyNavigation=services&category=watson).
+# - Select the service you want, **Discovery**, under the **Watson** category.
+# - Enter a Service Name for that instance, e.g. **Disco1** and a Credential Name, e.g. **Disco1-Creds** (these are just for you to be able to refer to later, they do not affect the functioning of the service).
+# - You should be able to see your newly-created service in your [Services Dashboard](https://console.ng.bluemix.net/dashboard/services).
+# - Open the service instance, click on the **Service credentials** tab, and then **View credentials** under Actions. This is where you will find the username and password to use when connecting to the service.
+# 
+# <img src="images/discovery-creds.png" alt="Discovery Service - Credentials tab" width="800" />
+# 
+# Save the credentials for the discovery service in a JSON file in the current directory named `service-credentials.json` with the following format:
+# 
+# ```json
+# {
+#     "discovery": {
+#         "username": "<your Discovery username here>",
+#         "password": "<your Discovery password here>"
+#     },
+#     "conversation": {
+#         "username": "",
+#         "password": ""
+#     }
+# }
+# 
+# ```
+# 
+# You will be filling out the Conversation service credentials later, when you create an instance for it. Note that you should keep these credentials secret. Please do not turn them in with your submission!
+# 
+# ### Connect to the service instance
+# 
+# Let's connect to the service instance you just created using IBM Watson's [Python SDK](https://github.com/watson-developer-cloud/python-sdk). You will first need to install the SDK:
+# ```bash
+# pip install watson-developer-cloud
+# ```
+# 
+# Now execute each code cell below using **`Shift+Enter`**, and complete any steps indicated by a **`TODO`** comment. For more information on the Discovery service, please read the [Documentation](https://www.ibm.com/watson/developercloud/doc/discovery/index.html) and look at the [API Reference](https://www.ibm.com/watson/developercloud/discovery/api/v1/?python) as needed.
+
+# In[1]:
+
+
+# Usual Python imports
+import sys
+import os
+import glob
+import json
+
+# BeautifulSoup, for parsing HTML
+from bs4 import BeautifulSoup
+
+# Matplotlib, for plotting
+import matplotlib.pyplot as plt
+get_ipython().magic(u'matplotlib inline')
+
+# Watson Python SDK
+import watson_developer_cloud
+
+# Utility functions
+import helper
+
+
+# In[2]:
+
+
+# Connect to the Discovery service instance
+discovery_creds = helper.fetch_credentials('discovery')
+discovery = watson_developer_cloud.DiscoveryV1(
+    version='2016-11-07',
+    username=discovery_creds['username'],
+    password=discovery_creds['password'])
+
+
+# ### Create an environment
+# 
+# The Discovery service organizes everything needed for a particular application in an _environment_. Let's create one called "Bookworm" for this project.
+# 
+# > _**Note**: It is okay to run this block multiple times - it will not create duplicate environments with the same name._
+
+# In[3]:
+
+
+# Prepare an environment to work in
+env, env_id = helper.fetch_object(
+    discovery, "environment", "Bookworm",
+    create=True, create_args=dict(
+        description="A space to read and understand stories",  # feel free to edit
+        size=0  # use 0 for free plan (see API reference for more on sizing)
+    ))
+print(json.dumps(env, indent=2))
+
+
+# In[4]:
+
+
+# View default configuration
+cfg_id = discovery.get_default_configuration_id(environment_id=env_id)
+cfg = discovery.get_configuration(environment_id=env_id, configuration_id=cfg_id)
+print(json.dumps(cfg, indent=2))
+
+
+# In[5]:
+
+
+# Test configuration on some sample text
+data_dir = "data"
+filename = os.path.join(data_dir, "sample.html")
+with open(filename, "r") as f:
+    res = discovery.test_document(environment_id=env_id, configuration_id=cfg_id, fileinfo=f)
+print(json.dumps(res, indent=2))
+
+
+# ### Analyze test output
+# 
+# The results returned by the service contain a _snapshot_ of the information extracted at each step of processing - document conversions, enrichments and normalizations. We are interested in the output of applying enrichments (`"enrichments_output"`) or after normalizing them (`"normalizations_output"`). These should be identical if no post-processing/normalizations were specified in the configuration.
+# results from the "enrichments_output" or "normalizations_output" step
+output = next((s["snapshot"] for s in res["snapshots"] if s["step"] == "enrichments_output"), None)
+print(json.dumps(output, indent=2))
+# In[7]:
+
+
+# Visualize keywords by relevance as a wordcloud
+from wordcloud import WordCloud
+
+wc_data = { w["text"]: w["relevance"] for w in output["enriched_text"]["keywords"] }
+wc = WordCloud(width=400, height=300, scale=2, background_color="white", colormap="Vega10")
+wc.generate_from_frequencies(wc_data)  # use precomputed relevance instead of frequencies
+
+plt.figure(figsize=(4, 3), dpi=200)
+plt.imshow(wc, interpolation='bilinear')
+plt.axis("off")
+
+
+# In[8]:
+
+
+# Prepare a collection of documents to use
+col, col_id = helper.fetch_object(discovery, "collection", "Story Chunks", environment_id=env_id,
+    create=True, create_args=dict(
+        environment_id=env_id, configuration_id=cfg_id,
+        description="Stories and plots split up into chunks suitable for answering"
+    ))
+print(json.dumps(col, indent=2))
+
+
+# In[9]:
+
+
+# Add documents to collection
+doc_ids = []  # to store the generated id for each document added
+for filename in glob.glob(os.path.join(data_dir, "Star-Wars", "*.html")):
+    print("Adding file:", filename)
+    with open(filename, "r") as f:
+        # Split each individual <p> into its own "document"
+        doc = f.read()
+        soup = BeautifulSoup(doc, 'html.parser')
+        for i, p in enumerate(soup.find_all('p')):
+            doc_info = discovery.add_document(environment_id=env_id, collection_id=col_id,
+                file_data=json.dumps({"text": p.get_text(strip=True)}),
+                mime_type="application/json",
+                metadata={"title": soup.title.get_text(strip=True)})
+            doc_ids.append(doc_info["document_id"])
+print("Total", len(doc_ids), "documents added.")
+
+
+# In[10]:
+
+
+# View collection details to verify all documents have been processed
+col, col_id = helper.fetch_object(discovery, "collection", "Story Chunks", environment_id=env_id)
+print(json.dumps(col, indent=2))
+
+
+# In[11]:
+
+
+# List all fields extracted
+discovery.list_collection_fields(environment_id=env_id, collection_id=col_id)
+
+
+# In[12]:
+
+
+# A simple query
+results = discovery.query(environment_id=env_id, collection_id=col_id,
+    query_options={
+        "query": "enriched_text.relations.subject.text:\"Jar Jar\"",
+        "return": "metadata.title,text"
+    })
+print(json.dumps(results, indent=2))
+
+
+# In[13]:
+
+
+# A simple query
+results = discovery.query(environment_id=env_id, collection_id=col_id,
+    query_options={
+        "query": "enriched_text.relations.subject.text:\"Obi-Wan Kenobi\"",
+        "return": "metadata.title,text"
+    })
+print(json.dumps(results, indent=2))
+
+
+# In[14]:
+
+
+# Connect to the Conversation service instance with your username and password from the Service Credentials tab in service-credentials.json
+conversation_creds = helper.fetch_credentials('conversation')
+conversation = watson_developer_cloud.ConversationV1(
+    version='2017-02-03',
+    username=conversation_creds['username'],
+    password=conversation_creds['password'])
+
+
+# Fetch the workspace you just created called "Bookworm".
+
+# In[15]:
+
+
+wrk, wrk_id = helper.fetch_object(conversation, "workspace", "Bookworm")
+print(json.dumps(wrk, indent=2))
+
+
+# Collect all the entities from the Discovery service collection.
+
+# In[16]:
+
+
+# all the entities from the collection and group them by type
+response = discovery.query(environment_id=env_id, collection_id=col_id,
+    query_options={
+        "return": "enriched_text.entities.type,enriched_text.entities.text"
+    })
+
+# Group individual entities by type ("Person", "Location", etc.)
+entities_by_type = {}
+for document in response["results"]:
+    for entity in document["enriched_text"]["entities"]:
+        if entity["type"] not in entities_by_type:
+            entities_by_type[entity["type"]] = set()
+        entities_by_type[entity["type"]].add(entity["text"])
+
+# Ignore case to avoid duplicates
+for entity_type in entities_by_type:
+    entities_by_type[entity_type] = {
+        e.lower(): e for e in entities_by_type[entity_type]
+    }.values()
+
+# Restructure for loading into Conversation workspace
+entities_grouped = [{
+    "entity": entity_type,
+    "values": [{"value": entity} for entity in entities]}
+        for entity_type, entities in entities_by_type.items()]
+entities_grouped
+
+
+# Update the workspace with these entities and verify that have been added correctly.
+
+# In[17]:
+
+
+#grouped entities to the Conversation workspace
+conversation.update_workspace(workspace_id=wrk_id, entities=entities_grouped)
+
+workspace_details = conversation.get_workspace(workspace_id=wrk_id, export=True)
+print(json.dumps(workspace_details["entities"], indent=2))
+
+
+# ### Test dialog
+# 
+# Let's run through a test dialog to demonstrate how the system transitions to one of the nodes you defined above.
+
+# In[18]:
+
+
+# Testing the dialog flow
+
+# Start conversation with a blank message
+results = conversation.message(workspace_id=wrk_id, message_input={})
+context = results["context"]
+
+# ask a sample question
+question= "Who is Luke's father?"
+results = conversation.message(workspace_id=wrk_id, message_input={
+    "text": question,
+    "context": context
+})
+print(json.dumps(results, indent=2))
+
+
+# In[19]:
+
+
+
+# sample question through Conversation service
+
+question= "Who killed Han Solo?"
+results = conversation.message(workspace_id=wrk_id, message_input={
+    "text": question,
+    "context": context
+})
+print(json.dumps(results, indent=2))
+
+
+# In[20]:
+
+
+# intent(s) the user expressed (typically a single one)
+query_intents = [intent["intent"] for intent in results["intents"]]
+print("Intent(s):", query_intents)
+
+# entities found in the question text
+query_entities = [entity["value"] for entity in results["entities"]]
+print("Entities:", query_entities)
+
+# dialog node was triggered
+query_dialognodes = [entity["entity"] for entity in results["entities"]]
+print("Dialognodes:", query_dialognodes)
+
+
+# In[21]:
+
+
+query_results = discovery.query(environment_id=env_id, collection_id=col_id,
+    query_options={
+        "query": "text:{}".format(",".join("\"{}\"".format(e) for e in query_entities)),
+        "return": "enriched_text.entities.type,enriched_text.entities.text,enriched_text.entities.relevance"
+    })
+print(json.dumps(query_results, indent=2))
+
+
+# In[22]:
+
+
+
+
+# top_entities returns the enriched_text.entities in the paragraph with the highest score
+# that wasn't an entity from the original query
+top_entities = [query['text'] for query in query_results['results'][0]['enriched_text']['entities']                 if query['text'] not in query_entities]
+# return the highest ranked
+print(question)
+print(top_entities[0])
+
